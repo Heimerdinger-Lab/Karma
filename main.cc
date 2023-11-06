4f1839be4555af0e8cf7e37b058d90c5e171bf33
tianpingan <tianpingan2000@gmail.com>
2023-11-06 07:35:03 +0000

    use asio

--- main.cc
+++ main.cc
@@ -1,29 +1,10 @@
-#include <co_context/io_context.hpp>
-#include <co_context/lazy_io.hpp>
-using namespace co_context;
+#include <asio.hpp> 
+#include <iostream>
 
-task<> cycle(int sec, const char *message) {
-    while (true) {
-        co_await timeout(std::chrono::seconds{sec});
-        printf("%s\n", message);
-    }
-}
-
-task<> cycle_abs(int sec, const char *message) {
-    auto next = std::chrono::steady_clock::now();
-    while (true) {
-        next = next + std::chrono::seconds{sec};
-        co_await timeout_at(next);
-        printf("%s\n", message);
-    }
-}
-
-int main() {
-    io_context ctx;
-    ctx.co_spawn(cycle(1, "1 sec"));
-    ctx.co_spawn(cycle_abs(1, "1 sec [abs]"));
-    ctx.co_spawn(cycle(3, "\t3 sec"));
-    ctx.start();
-    ctx.join();
+int main()
+{
+    asio::io_context io_context;
+    io_context.run();
+    std::cout << "Do you reckon this line displays?" << std::endl;
     return 0;
 }
