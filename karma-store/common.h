1c701088ef0f364f3eb9d48eabc97a2184bca551
tianpingan <tianpingan2000@gmail.com>
2024-03-04 14:51:51 +0000

    [fix] polish the karma-store and fix some exception

---
+++ karma-store/common.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <cstdint>
+#include <memory>
+#include <span>
+#include <variant>
+
+#include "co_context/co/channel.hpp"
+#include "karma-store/buf/aligned_buf.h"
+namespace store {
+// crc32(4 Bytes) + length_type(4 Bytes)
+const uint64_t RECORD_HEADER_LENGTH = 4 + 4;
+enum uring_op { read, write };
+struct record_pos {
+    uint64_t start_wal_offset;
+    uint64_t record_size;
+};
+struct read_io_result {
+    bool success;
+};
+struct write_io_result {
+    bool success;
+    uint64_t start_wal_offset;
+    uint64_t size;
+};
+struct read_io {
+    uint64_t start_wal_offset;
+    uint64_t size;
+    std::string &data;
+    //
+    co_context::channel<read_io_result> &m_prom;
+    co_context::io_context &m_ctx;
+};
+struct write_io {
+    std::span<char> data;
+    //
+    co_context::channel<write_io_result> &m_prom;
+    co_context::io_context &m_ctx;
+};
+
+struct write_uring_context {
+    // 整个buffer都刷下去
+    // commit [buffer.wal_offset, buffer.limit())
+    std::shared_ptr<aligned_buf> buffer;
+};
+struct read_uring_context {
+    std::shared_ptr<aligned_buf> buffer;
+    uint64_t read_seq;
+};
+struct uring_context {
+    uring_op opcode;
+    std::variant<write_uring_context, read_uring_context> detail;
+};
+};  // namespace store
